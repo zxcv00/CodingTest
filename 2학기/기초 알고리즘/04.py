0419584899e6a6278636faf6872c5d72d8d06cce
cat88 <w2012@e-mirim.hs.kr>
2021-11-01 09:31:06 +0900

    Bubble Sort 구현

--- 2학기/기초 알고리즘/04.py
+++ 2학기/기초 알고리즘/04.py
@@ -2,38 +2,45 @@
 
 arr = [8, 5, 6, 3, 4]
 
-# 첫 번째
-if arr[0] > arr[1]:
-    arr[0], arr[1] = arr[1], arr[0]
-
-if arr[1] > arr[2]:
-    arr[1], arr[2] = arr[2], arr[1]
-
-if arr[2] > arr[3]:
-    arr[2], arr[3] = arr[3], arr[2]
-
-if arr[3] > arr[4]:
-    arr[3], arr[4] = arr[4], arr[3]
-
-# 두 번째
-if arr[0] > arr[1]:
-    arr[0], arr[1] = arr[1], arr[0]
-
-if arr[1] > arr[2]:
-    arr[1], arr[2] = arr[2], arr[1]
-
-if arr[2] > arr[3]:
-    arr[2], arr[3] = arr[3], arr[2]
-
-# 세 번째
-if arr[0] > arr[1]:
-    arr[0], arr[1] = arr[1], arr[0]
-
-if arr[1] > arr[2]:
-    arr[1], arr[2] = arr[2], arr[1]
-
-# 네 번째
-if arr[0] > arr[1]:
-    arr[0], arr[1] = arr[1], arr[0]
+for i in range(0, len(arr)-1):
+    for j in range(len(arr)-i-1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 print(arr)
+
+
+
+# 첫 번째
+# if arr[0] > arr[1]:
+#     arr[0], arr[1] = arr[1], arr[0]
+#
+# if arr[1] > arr[2]:
+#     arr[1], arr[2] = arr[2], arr[1]
+#
+# if arr[2] > arr[3]:
+#     arr[2], arr[3] = arr[3], arr[2]
+#
+# if arr[3] > arr[4]:
+#     arr[3], arr[4] = arr[4], arr[3]
+#
+# 두 번째
+# if arr[0] > arr[1]:
+#     arr[0], arr[1] = arr[1], arr[0]
+#
+# if arr[1] > arr[2]:
+#     arr[1], arr[2] = arr[2], arr[1]
+#
+# if arr[2] > arr[3]:
+#     arr[2], arr[3] = arr[3], arr[2]
+#
+# 세 번째
+# if arr[0] > arr[1]:
+#     arr[0], arr[1] = arr[1], arr[0]
+#
+# if arr[1] > arr[2]:
+#     arr[1], arr[2] = arr[2], arr[1]
+#
+# 네 번째
+# if arr[0] > arr[1]:
+#     arr[0], arr[1] = arr[1], arr[0]
